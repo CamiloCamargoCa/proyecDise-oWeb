e7bf73f539216bc2958569f43e38d840b2d611d6
Camilo Camargo <kfl340@hotmail.com>
2018-11-19 22:09:44 -0500

    inicio de proyecto

---
+++ Modelos/conexionbd.php
@@ -0,0 +1,21 @@
+<?php 
+class Conexion extends PDO{
+
+
+    public function __construct(){
+    
+    
+        try{
+            parent::__construct('mysql:host=localhost;dbname=essen','root','');
+            parent::setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+            } catch(Exeption $ex){
+                echo "Error con la base de datos : ".$ex->getMessage();
+            }
+        
+        }
+    }
+
+
+
+
+?>
